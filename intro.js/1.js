b3da4dd481d9c1f9c701b6cf33bf944d146e896a
Anshika <anshika2020ag@gmail.com>
2023-12-16 12:17:46 +0530

    basic introduction to node js

---
+++ intro.js/1.js
@@ -0,0 +1,14 @@
+const i = 10;
+if (i<5) {
+    console.log("small number");
+}
+else {
+    console.log("large number");
+}
+
+console.log("let's try to run this basic node js app.");
+
+console.log("directory name: " + __dirname);
+console.log("file name: " + __filename);
+console.log("environment: " + process);
+console.log("module: " + module);
