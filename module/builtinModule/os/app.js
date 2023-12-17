ca99a9d939ddbdb409378969a9063a9c1ce7ef3a
Anshika <anshika2020ag@gmail.com>
2023-12-18 01:59:45 +0530

    os module

--- module/builtinModule/os/app.js
+++ module/builtinModule/os/app.js
@@ -1,4 +1,8 @@
-const os = require('node:os');
+const os = require('os');
 console.log(os.arch())
 // console.log(os.cpus())
 console.log(os.devNull)
+console.log(os.availableParallelism());
+console.log(os.freemem()/1024/1024/1024);
+console.log("System uptime: " + os.uptime + " seconds.");
+console.log("Total memory: " + os.totalmem() + " bytes.")
