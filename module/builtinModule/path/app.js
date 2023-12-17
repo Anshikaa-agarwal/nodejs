23f19a2e82234b2befb5e49df0a263ed3cf7d47a
Anshika <anshika2020ag@gmail.com>
2023-12-18 02:17:26 +0530

    path module

--- module/builtinModule/path/app.js
+++ module/builtinModule/path/app.js
@@ -1,1 +1,3 @@
 const path = require('path');
+console.log(`Basename: ${path.basename("C://Users//Anshika//OneDrive//Desktop//node//nodejs//module//builtinModule//path//app.js")}`);
+console.log(`Delimeter: ${path.delimiter}`)
