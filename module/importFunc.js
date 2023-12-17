59fe9bde78a8f8d4492da5aadb506f964f4050ce
Anshika <anshika2020ag@gmail.com>
2023-12-17 18:33:33 +0530

    importing self modules

--- module/importFunc.js
+++ module/importFunc.js
@@ -1,7 +1,13 @@
 //modules in js
 //we'll import exportFunc.js here.
-const impFunc = require('./exportFunc.js');
-console.log(impFunc);
-console.log(impFunc.greetName);
-console.log(impFunc.name1);
-console.log(impFunc.name2);
+// const impFunc = require('./exportFunc.js');
+// console.log(impFunc);
+// console.log(impFunc.greetName);
+// console.log(impFunc.name1);
+// console.log(impFunc.name2);
+
+
+//another way to import
+const {name2, greetName} = require('./exportFunc.js');
+console.log(greetName);
+console.log(name2);
