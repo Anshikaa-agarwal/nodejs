382c7dcdb7349d4b21c25f3c35ab8c3cff7521bc
Anshika <anshika2020ag@gmail.com>
2023-12-18 02:03:52 +0530

    os

--- module/builtinModule/os/app.js
+++ module/builtinModule/os/app.js
@@ -3,6 +3,8 @@ console.log(os.arch())
 // console.log(os.cpus())
 console.log(os.devNull)
 console.log(os.availableParallelism());
-console.log(os.freemem()/1024/1024/1024);
+console.log(os.freemem());
 console.log("System uptime: " + os.uptime + " seconds.");
-console.log("Total memory: " + os.totalmem() + " bytes.")
+console.log("Total memory: " + os.totalmem() + " bytes.");
+console.log("type of operationg system i'm using: " + os.type);
+console.log("Kernel version: " + os.version)
