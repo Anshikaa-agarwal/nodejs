463143ea674844bf33be1e3e216bd0b1729e3dd2
Anshika <anshika2020ag@gmail.com>
2023-12-18 15:34:23 +0530

    npm chalk

---
+++ npm/chalk.mjs
@@ -0,0 +1,8 @@
+import chalk from 'chalk';
+
+
+console.log(chalk.blue("hello"));
+console.log(chalk.bgBlueBright("hello"));
+console.log(chalk.bgMagentaBright("hello"));
+console.log(chalk.bgMagenta("hello"));
+console.log(chalk.italic("hello"))
