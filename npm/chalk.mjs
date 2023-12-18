ac8faca992a46fab3bf57f484e487c05f098027a
Anshika <anshika2020ag@gmail.com>
2023-12-18 16:03:02 +0530

    npm

--- npm/chalk.mjs
+++ npm/chalk.mjs
@@ -1,8 +1,11 @@
 import chalk from 'chalk';
-
+import validator from 'validator';
 
 console.log(chalk.blue("hello"));
 console.log(chalk.bgBlueBright("hello"));
 console.log(chalk.bgMagentaBright("hello"));
 console.log(chalk.bgMagenta("hello"));
-console.log(chalk.italic("hello"))
+console.log(chalk.italic("hello"));
+
+console.log(validator.isEmail('abcd@hjk.nm'));
+console.log(validator.isBoolean("110"));
