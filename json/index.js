6f307b619c226a4f2a5495f9fe3e5c60729c3aed
Anshika <anshika2020ag@gmail.com>
2023-12-24 15:18:31 +0530

    json

--- json/index.js
+++ json/index.js
@@ -1,3 +1,4 @@
+const fs = require("fs");
 const biodata = {
     name: "abc",
     age: 19
@@ -10,3 +11,30 @@ console.log(jsonData);
 // to convert json back to object, we use parse:
 const objData = JSON.parse(jsonData);
 console.log(objData);
+
+// Tasks to do:
+// 1. covert object to JSON
+// 2. add it to another file
+// 3. read file
+// 4. convert back to original js object
+// 5. console.log()
+
+const obj = {
+    name: "Anshika",
+    age: 19,
+    college: "jiit"
+};
+
+const jsonD = JSON.stringify(obj);
+
+fs.writeFile('jsonFile.json', jsonD, (err) => {
+    console.log('action completed');
+})
+
+const original = JSON.parse(jsonD);
+
+console.log(original);
+
+fs.readFile("jsonFile.json", 'utf-8', (err, data) => {
+    console.log(data);
+})
