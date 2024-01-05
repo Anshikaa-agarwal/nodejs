3385d2dfa7dccdcf60634376798cd61405dde29e
Anshika <anshika2020ag@gmail.com>
2024-01-06 00:05:32 +0530

    stream method 3

---
+++ stream/3.js
@@ -0,0 +1,13 @@
+//another stream method o read file and show data
+
+const fs = require('fs');
+const http = require('http');
+
+const server = http.createServer();
+
+server.on('request', (req, res) => {
+    const rstream = fs.createReadStream('textfile.txt');
+    rstream.pipe(res);
+});
+
+server.listen(8000, '127.0.0.1');
